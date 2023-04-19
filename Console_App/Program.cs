275c55420ae26e6535bd0f4b704d6c5ec5860b46
Inga666 <sergievskyns@mail.ru>
2023-04-20 00:18:25 +0700

    FirstDLL

---
+++ Console_App/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using FirstDLL;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double i, j;
+            i = Convert.ToDouble(Console.ReadLine());
+            j = Convert.ToDouble(Console.ReadLine());
+            Class1.SumFunc(i, j);
+        }
+    }
+}
